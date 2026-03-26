ca6c5ebb7f4ff251a45b07af63cefb4cd4bc76ee
AadityaMishra1 <amishr26@ncsu.edu>
2026-03-25 23:14:01 -0400

    Update Gilfoyle to v0.2.1

--- Casks/gilfoyle.rb
+++ Casks/gilfoyle.rb
@@ -1,6 +1,6 @@
 cask "gilfoyle" do
-  version "0.2.0"
-  sha256 "314d3439783e99207b2d8c308104271b8499e3fe0543619129f3e8503ebf85c4"
+  version "0.2.1"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/AadityaMishra1/Gilfoyle/releases/download/v#{version}/Gilfoyle-#{version}-universal.dmg"
   name "Gilfoyle"
